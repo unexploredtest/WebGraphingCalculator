90a71136f12592bce692f90c2afa322630716770
aliPMPAINT <53617231+aliPMPAINT@users.noreply.github.com>
2020-02-09 17:45:25 +0330

    Created Graph Database
    Created Graph Database for graphs

--- Website/calculator/models.py
+++ Website/calculator/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Graph(models.Model):
+
+    equation = models.CharField(max_length=30)
+    description = models.TextField()
+    image = models.ImageField()
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
