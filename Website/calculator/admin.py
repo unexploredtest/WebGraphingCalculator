5ef70d88beef3ed10abe5dd2674dad39cb636d74
aliPMPAINT <53617231+aliPMPAINT@users.noreply.github.com>
2020-02-10 18:35:45 +0330

    Improved the Graph database

--- Website/calculator/admin.py
+++ Website/calculator/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Graph
 
-# Register your models here.
+admin.site.register(Graph)
--- Website/calculator/models.py
+++ Website/calculator/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Graph(models.Model):
 
-    equation = models.CharField(max_length=30)
+    equation = models.CharField(max_length=30, primary_key=True)
     description = models.TextField()
     image = models.ImageField(upload_to='graphs/')
 
